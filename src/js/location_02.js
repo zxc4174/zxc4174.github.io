cbfdab83a0bc0350382fe81a70fec23c70f95f1f
yuhsianglo <zxc4174@gmail.com>
2021-03-06 18:14:12 +0900

    feat : add poem modal

--- src/js/location_02.js
+++ src/js/location_02.js
@@ -8,6 +8,8 @@ panorama = new PANOLENS.ImagePanorama('../images/location_02/bg_02.png');
 let infospot = new PANOLENS.Infospot(600, '../images/location_02/can_or_not.png');
 infospot.position.set(500, -700, 1000);
 infospot.addEventListener('hover', () => {
+    const div = document.getElementById('poem');
+    div.innerHTML += '不可以';
     document.querySelector('#can_or_not_audio').play();
 });
 
@@ -15,6 +17,8 @@ infospot.addEventListener('hover', () => {
 let infospot2 = new PANOLENS.Infospot(350, '../images/location_02/full.png');
 infospot2.position.set(1300, 450, 1000);
 infospot2.addEventListener('hover', () => {
+    const div = document.getElementById('poem');
+    div.innerHTML += '吃飽了';
     document.querySelector('#full_audio').play();
 });
 
@@ -22,6 +26,8 @@ infospot2.addEventListener('hover', () => {
 let infospot3 = new PANOLENS.Infospot(3000, '../images/location_02/how_much.png');
 infospot3.position.set(-2800, 5500, 2000);
 infospot3.addEventListener('hover', () => {
+    const div = document.getElementById('poem');
+    div.innerHTML += '今天是賺多少錢';
     document.querySelector('#how_much_audio').play();
 });
 
@@ -29,6 +35,8 @@ infospot3.addEventListener('hover', () => {
 let infospot4 = new PANOLENS.Infospot(3000, '../images/location_02/later_tonight.png');
 infospot4.position.set(-8500, 2300, 2000);
 infospot4.addEventListener('hover', () => {
+    const div = document.getElementById('poem');
+    div.innerHTML += '今天好像比較晚';
     document.querySelector('#later_tonight_audio').play();
 });
 
@@ -36,25 +44,27 @@ infospot4.addEventListener('hover', () => {
 let infospot5 = new PANOLENS.Infospot(4000, '../images/location_02/what_drinks_you_want.png');
 infospot5.position.set(8000, 5000, 1000);
 infospot5.addEventListener('hover', () => {
+    const div = document.getElementById('poem');
+    div.innerHTML += '你要喝什麼飲料';
     document.querySelector('#what_drinks_you_want_audio').play();
 });
 
 panorama.add(infospot, infospot2, infospot3, infospot4, infospot5);
 
 viewer = new PANOLENS.Viewer({
-    container: container,
-    // autoRotate: true,
-    // autoRotateSpeed: 1,
-    // autoRotateActivationDuration: 5000
+    container: container
 });
 viewer.add(panorama);
 
-$(window).on('load', () => {
-    $('.loading_box').fadeOut("slow").hide();
-    $('#entry_modal').modal('show');
-});
+// $(window).on('load', () => {
+//     $('.loading_box').fadeOut("slow").hide();
+//     $('#entry_modal').modal('show');
+// });
 
-$("#entry_modal").on("hidden.bs.modal", () => {
-    $('#bg_audio').get(0).play();
-});
+// $("#entry_modal").on("hidden.bs.modal", () => {
+//     $('#bg_audio').get(0).play();
+// });
 
+// $('body').on('DOMSubtreeModified', '#poem', function () {
+//     $('#poem').show();
+// });
