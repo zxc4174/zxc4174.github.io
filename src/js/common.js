344036ee5634da0ee946bd79e237d8f57c308580
yuhsianglo <zxc4174@gmail.com>
2021-03-09 00:28:08 +0900

    feat : feat new map

---
+++ src/js/common.js
@@ -0,0 +1,29 @@
+$(window).on('load', () => {
+    $('.loading_box').fadeOut("slow").hide();
+    $('#entry_modal').modal('show');
+});
+
+$("#entry_modal").on("hidden.bs.modal", () => {
+    $('#bg_audio').get(0).play();
+});
+
+$('body').on('DOMSubtreeModified', '#poem', function () {
+    $('#poem').show();
+});
+
+$('#download').on('click', () => {
+    const content = $('#poem_content').text();
+    console.log(content);
+    $('#download').attr('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+});
+
+function appendHoverText(text) {
+    const poem_content = document.getElementById('poem_content');
+    poem_content.innerHTML += text;
+    const div = document.getElementById('poem');
+    div.scrollTop = div.scrollHeight;
+}
+
+function playTextAudio(targetId) {
+    document.querySelector('#' + targetId).play();
+}
